912b03f5795ece07ce4946d030c7b6c3cbb4c783
Almog-G <83763282+almoggv@users.noreply.github.com>
2021-10-30 21:46:11 +0300

    inital upload of project files

---
+++ IIdable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioCodes_First_Task___Almog_Geva
+{
+    public interface IIdable
+    {
+        string Id { get; }
+    }
+}
